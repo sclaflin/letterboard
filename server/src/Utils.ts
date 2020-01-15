140f7e42ba2b215671d97514485f5d0ac3f0884f
Sean Claflin <seanclaflin@gmail.com>
2020-01-15 07:28:56 -0600

    Added helper method to validate parameters during runtime.

--- server/src/Utils.ts
+++ server/src/Utils.ts
@@ -2,3 +2,11 @@ export function errConcat(src: Error, target: Error): Error {
 	target.stack += '\nCaused By:\n'+ src.stack;
 	return target;
 }
+export function validateParam(param: any, name: string, primitives?: string[] | null, instances?: Function[] | null): void {
+	if(primitives && primitives.indexOf(typeof param) === -1) {
+		throw new TypeError(name +' must be a: '+ primitives.join(', '));
+	}
+	if(instances && instances.filter(v => param instanceof v).length === 0) {
+		throw new TypeError(name +' must be an instance of: '+ instances.map(v => v.name));
+	}
+}
