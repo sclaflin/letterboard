d1fcf4eec12175e52077358d5f8a55454087f1fc
Sean Claflin <seanclaflin@gmail.com>
2020-01-15 10:58:45 -0600

    Added swagger documentation inline.

--- server/src/LetterService.ts
+++ server/src/LetterService.ts
@@ -1,5 +1,6 @@
 import * as restify from 'restify';
 import * as corsMiddleware from 'restify-cors-middleware';
+import * as restifySwaggerJsDoc from 'restify-swagger-jsdoc';
 import { Style } from './Style';
 import { Letter } from './Letter';
 import { Database } from './Database';
@@ -41,10 +42,117 @@ export class LetterService {
 		this._server.use(restify.plugins.acceptParser(this._server.acceptable));
 		this._server.use(restify.plugins.queryParser());
 		this._server.use(restify.plugins.bodyParser());
+		restifySwaggerJsDoc.createSwaggerPage({
+			title: 'LetterService API',
+			description: 'LetterService API information',
+			version: '0.0.0',
+			server: this._server,
+			path: '/api-docs',
+			apis: ['**/*.ts'],
+			definitions: {
+				Style: {
+					type: 'object',
+					properties: {
+						backgroundColor: {
+							type: 'string'
+						},
+						border: {
+							type: 'string'
+						},
+						color: {
+							type: 'string'
+						},
+						fontSize: {
+							type: 'string'
+						},
+						width: {
+							type: 'string'
+						},
+						height: {
+							type: 'string'
+						},
+						position: {
+							type: 'string'
+						},
+						left: {
+							type: 'string'
+						},
+						top: {
+							type: 'string'
+						},
+						transform: {
+							type: 'string'
+						},
+						transformOrigin: {
+							type: 'string'
+						},
+						margin: {
+							type: 'string'
+						},
+						boxShadow: {
+							type: 'string'
+						}
+					}
+				},
+				Letter: {
+					type: 'object',
+					properties: {
+						id: {
+							type: 'integer',
+							format: 'int64'
+						},
+						value: {
+							type: 'string',
+							minLength: 1,
+							maxLength: 1
+						},
+						style: {
+							$ref: '#/definitions/Style'
+						}
+					},
+					required: ['id']
+				},
+				ArrayOfLetter: {
+					type: 'array',
+					items: {
+						$ref: '#/definitions/Letter'
+					}
+				}
+			}
+		});
+		/**
+		 * @swagger
+		 *
+		 * /:
+		 *   get:
+		 *     summary: Says hi!
+		 *     produces:
+		 *       - application/json
+		 *     responses:
+		 *       200:
+		 *         description: Friendly greeting.
+		 */
 		this._server.get('/', async (req, res, next) => {
 			res.send('Hi!');
 			return next();
 		});
+
+		/**
+		 * @swagger
+		 *
+		 * /board:
+		 *   get:
+		 *     tags:
+		 *       - Board
+		 *     summary: Returns the board CSS style properties
+		 *     produces:
+		 *       - application/json
+		 *     responses:
+		 *       200:
+		 *         description: OK
+		 *         schema:
+		 *           $ref: '#/definitions/Style'
+		 */
 		this._server.get('/board', async (req, res, next) => {
 			try {
 				res.send(await this._database.getBoard());
@@ -52,6 +160,23 @@ export class LetterService {
 			}
 			catch(e) { return next(e); }
 		});
+
+		/**
+		 * @swagger
+		 *
+		 * /letters:
+		 *   get:
+		 *     tags:
+		 *       - Letter
+		 *     summary: Returns all Letter properties
+		 *     produces:
+		 *       - application/json
+		 *     responses:
+		 *       200:
+		 *         description: OK
+		 *         schema:
+		 *           $ref: '#/definitions/ArrayOfLetter'
+		 */
 		this._server.get('/letters', async (req, res, next) => {
 			try {
 				res.send(await this._database.getLetters());
@@ -59,6 +184,32 @@ export class LetterService {
 			}
 			catch(e) { return next(e); }
 		});
+
+		/**
+		 * @swagger
+		 *
+		 * /letters/{index}:
+		 *   get:
+		 *     tags:
+		 *       - Letter
+		 *     summary: Returns proprties for letter by index
+		 *     parameters:
+		 *       - name: index
+		 *         in: path
+		 *         required: true
+		 *         description: The index number of the letter
+		 *         schema:
+		 *           type: integer
+		 *           format: int32
+		 *           minimum: 0
+		 *     produces:
+		 *       - application/json
+		 *     responses:
+		 *       200:
+		 *         description: OK
+		 *         schema:
+		 *           $ref: '#/definitions/Letter'
+		 */
 		this._server.get('/letters/:index', async (req, res, next) => {
 			try {
 				const index: number = Number(req.params.index);
@@ -67,6 +218,39 @@ export class LetterService {
 			}
 			catch(e) { return next(e); }
 		});
+
+		/**
+		 * @swagger
+		 *
+		 * /letters/{index}:
+		 *   put:
+		 *     tags:
+		 *       - Letter
+		 *     summary: Updates Letter properties
+		 *     description: Only Style.left, Style.top, Style.transform, and Style.transformOrigin properties will be updated.
+		 *     parameters:
+		 *       - in: path
+		 *         name: index
+		 *         required: true
+		 *         description: The index number of the letter
+		 *         schema:
+		 *           type: integer
+		 *           format: int32
+		 *           minimum: 0
+		 *       - in: body
+		 *         name: letter
+		 *         required: true
+		 *         description: The letter to update.
+		 *         schema:
+		 *           $ref: '#/definitions/Letter'
+		 *     produces:
+		 *       - application/json
+		 *     responses:
+		 *       200:
+		 *         description: OK
+		 *         schema:
+		 *           $ref: '#/definitions/Letter'
+		 */
 		this._server.put('/letters/:index', async (req, res, next) => {
 			try {
 				const index: number = Number(req.params.index);
